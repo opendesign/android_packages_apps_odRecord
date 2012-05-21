87ad87137751e4f292cb1b6f57ff35138a3f4324
XpLoDWilD <xplod@ouverta.fr>
2012-05-21 22:28:50 +0200

    Add playback button and share intent

--- src/com/opendesign/record/OdRecordActivity.java
+++ src/com/opendesign/record/OdRecordActivity.java
@@ -19,17 +19,24 @@ import java.util.concurrent.TimeUnit;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class OdRecordActivity extends Activity {
     private AudioRecorder mRecorder = null;
     private Timer mUiTimer = null;
     private long mRecordingStartTime = 0;
+    private String mLastFileName = "";
+    private MediaPlayer mMediaPlayer = null;
 
     class UiUpdateTask extends TimerTask {
         public void run() {
@@ -38,15 +45,18 @@ public class OdRecordActivity extends Activity {
 
             vuMeter.setValue(mRecorder.getAmplitude());
 
-            final long duration = System.currentTimeMillis() - mRecordingStartTime;
-            
+            final long duration = System.currentTimeMillis()
+                    - mRecordingStartTime;
+
             runOnUiThread(new Runnable() {
                 public void run() {
-                    durationText.setText(String.format("%02d:%02d", 
+                    durationText.setText(String.format(
+                            "%02d:%02d",
                             TimeUnit.MILLISECONDS.toMinutes(duration),
-                            TimeUnit.MILLISECONDS.toSeconds(duration) - 
-                            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration))
-                        ));
+                            TimeUnit.MILLISECONDS.toSeconds(duration)
+                                    - TimeUnit.MINUTES
+                                            .toSeconds(TimeUnit.MILLISECONDS
+                                                    .toMinutes(duration))));
                     vuMeter.invalidate();
                 }
             });
@@ -58,6 +68,10 @@ public class OdRecordActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        ToggleButton playBtn = (ToggleButton) findViewById(R.id.btnPlayLastRecord);
+        playBtn.setEnabled(false);
+        playBtn.setAlpha(0.3f);
     }
 
     @Override
@@ -65,6 +79,34 @@ public class OdRecordActivity extends Activity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_activity, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.menu_share:
+            if (mLastFileName.equals("")) {
+                Toast.makeText(this,
+                        getResources().getString(R.string.no_recording_done),
+                        Toast.LENGTH_LONG).show();
+                return true;
+            }
+
+            Intent sharingIntent = new Intent(
+                    android.content.Intent.ACTION_SEND);
+            sharingIntent.setType("audio/3gpp");
+            sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,
+                    getResources().getString(R.string.share_title));
+            sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT,
+                    getResources().getString(R.string.share_body));
+            sharingIntent.putExtra(Intent.EXTRA_STREAM,
+                    Uri.parse("file:///sdcard/Recording/" + mLastFileName));
+            startActivity(Intent.createChooser(sharingIntent, getResources()
+                    .getString(R.string.share_dialog_title)));
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
     }
 
     public void makeAlert(final String message) {
@@ -78,6 +120,29 @@ public class OdRecordActivity extends Activity {
         alert.show();
     }
 
+    public void onClickPlay(View v) {
+        if (mMediaPlayer == null) {
+            String PATH_TO_FILE = "/sdcard/Recording/" + mLastFileName;
+            mMediaPlayer = new MediaPlayer();
+    
+            try {
+                mMediaPlayer.setDataSource(PATH_TO_FILE);
+                mMediaPlayer.prepare();
+            } catch (IllegalArgumentException e) {
+                Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
+            } catch (IllegalStateException e) {
+                Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
+            } catch (IOException e) {
+                Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
+            }
+            
+            mMediaPlayer.start();
+        } else {
+            mMediaPlayer.stop();
+            mMediaPlayer = null;
+        }
+    }
+
     public void onClickRecord(View v) {
         final ToggleButton tb = (ToggleButton) findViewById(R.id.btnRecord);
 
@@ -89,8 +154,8 @@ public class OdRecordActivity extends Activity {
             // prepare recording
             SimpleDateFormat fileNameDate = new SimpleDateFormat();
             fileNameDate.applyPattern("yyyy-MM-dd HH.mm.ss");
-            mRecorder = new AudioRecorder(fileNameDate.format(new Date())
-                    + ".3gp");
+            mLastFileName = fileNameDate.format(new Date()) + ".3gp";
+            mRecorder = new AudioRecorder(mLastFileName);
 
             // start recording
             try {
@@ -115,6 +180,10 @@ public class OdRecordActivity extends Activity {
             try {
                 mUiTimer.cancel();
                 mRecorder.stop();
+                
+                ToggleButton playBtn = (ToggleButton) findViewById(R.id.btnPlayLastRecord);
+                playBtn.setEnabled(true);
+                playBtn.setAlpha(1f);
             } catch (IOException e) {
                 makeAlert("Error while stopping recording:\n" + e.getMessage());
                 tb.setChecked(true);
