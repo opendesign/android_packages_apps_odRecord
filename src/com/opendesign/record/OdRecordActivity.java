ed4a0aaeb8b1ba9bf50eb386815214072df10c3a
XpLoDWilD <xplod@ouverta.fr>
2012-05-21 17:45:42 +0200

    Initial import

---
+++ src/com/opendesign/record/OdRecordActivity.java
@@ -0,0 +1,118 @@
+/**
+ * Copyright (C) 2012 The OpenDESIGN Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package com.opendesign.record;
+
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.ToggleButton;
+
+public class OdRecordActivity extends Activity {	
+	private AudioRecorder mRecorder = null;
+	private Timer mVuMeterTimer = null;
+	
+	class VuMeterTimedTask extends TimerTask {
+		VuMeterView mVuMeter = null;
+		
+		public void run() {
+			if (mVuMeter == null) {
+				mVuMeter = (VuMeterView)findViewById(R.id.vuMeterView);
+			}
+			
+			mVuMeter.setValue(mRecorder.getAmplitude());
+			
+			runOnUiThread(new Runnable() {
+				public void run() {
+					mVuMeter.invalidate();
+				}
+			});
+			
+		}
+	}
+
+	
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.main_activity, menu);
+	    return true;
+	}
+	
+	public void makeAlert(final String message) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage(message)
+		.setCancelable(false)
+		.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id) {
+			}
+		});
+		AlertDialog alert = builder.create();
+		alert.show();
+	}
+
+
+	public void onClickRecord(View v) {
+		final ToggleButton tb = (ToggleButton)findViewById(R.id.btnRecord);
+
+		if (tb.isChecked()) {
+			// force disabled before recording
+			tb.setChecked(false);
+			tb.setEnabled(false);
+
+			// prepare recording
+			mRecorder = new AudioRecorder("test.3gp");
+
+			// start recording
+			try {
+				mRecorder.start();
+			} catch (IOException e) {
+				makeAlert("Error while starting recording:\n" + e.getMessage());
+				tb.setChecked(false);
+				tb.setEnabled(true);
+			} finally {
+				tb.setChecked(true);
+				tb.setEnabled(true);
+				
+				mVuMeterTimer =	new Timer();
+				mVuMeterTimer.schedule(new VuMeterTimedTask(), 10, 20);
+			}
+		} else {
+			tb.setChecked(true);
+			tb.setEnabled(false);
+
+			try {
+				mVuMeterTimer.cancel();
+				mRecorder.stop();
+			} catch (IOException e) {
+				makeAlert("Error while stopping recording:\n" + e.getMessage());
+				tb.setChecked(true);
+				tb.setEnabled(true);
+			} finally {
+				tb.setChecked(false);
+				tb.setEnabled(true);
+			}
+		}
+	}
+}
---
+++ src/com/opendesign/record/VuMeterView.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (C) 2012 The OpenDESIGN Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package com.opendesign.record;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+public class VuMeterView extends View {
+	int mCurrentValue = 0;
+	int mPeakValue = 0;
+	
+	final int mMaxValue = 26400;
+	
+	public VuMeterView(Context context, AttributeSet attrs, int defStyle) { 
+		super(context, attrs, defStyle); 
+	} 
+	public VuMeterView(Context context, AttributeSet attrs) { 
+		super(context, attrs); 
+	} 
+	public VuMeterView(Context context) { 
+		super(context); 
+	} 
+
+	public void setValue(int value) {
+		mCurrentValue = value;
+		
+		if (value > mPeakValue)
+			mPeakValue = value;
+	}
+	
+	public void resetPeak() {
+		mPeakValue = mCurrentValue;
+	}
+	
+	@Override 
+	protected void onDraw(Canvas canvas) { 
+		super.onDraw(canvas); 
+		
+		Paint paint = new Paint(); 
+		paint.setStyle(Paint.Style.STROKE);
+		paint.setStrokeWidth(20);
+		
+		// draw meter line
+		paint.setColor(0xFF99CC00);
+		canvas.drawLine(0, 0, mCurrentValue * getWidth() / mMaxValue, 0, paint);
+		
+		// draw peak
+		paint.setColor(0xFFCC0000); 
+		canvas.drawLine(getWidth() - 10, 0, getWidth(), 0, paint); 
+		
+		//Log.e("redraw", "redraw");
+	} 
+} 
